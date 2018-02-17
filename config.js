021c9d791a6353be2be7feb218804eb1fbffbc5a
liudmyla_schastlyvets <lyudmila.zoria@n3wnormal.com>
2018-02-17 14:38:06 +0200

    Changed mongodb url

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const host = 'localhost'
 
 module.exports = {
   port: process.env.port || process.env.PORT || '3005',
-  mongoUrl: process.env.MONGODB_URI || `mongodb://mila_schastlyvets:12345@ds237808.mlab.com:37808/todolist`
+  mongoUrl: process.env.MONGODB_URI || `mongodb://${host}/todolist`
 }
