97f57572df852dc64657fee7e850d0602163cff4
liudmyla_schastlyvets <lyudmila.zoria@n3wnormal.com>
2018-02-21 17:36:37 +0200

    Fixed db url

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const host = 'localhost'
 
 module.exports = {
   port: process.env.port || process.env.PORT || '3005',
-  mongoUrl: process.env.MONGODB_URI || `mongodb://mila:12345@ds145438.mlab.com:45438/eventlist`
+  mongoUrl: process.env.MONGODB_URI || `mongodb://${host}/eventlist`
 }
